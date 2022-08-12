9125fb6a0aa10cdcc5f1ef713a61cf769161f66d
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-12 18:32:05 -0300

    fix: refatora index para melhor leitura

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const rota = require('./controllers/productsControllers');
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
-app.get('/teste', (req, res) => {
-  res.status(200).send('<h1>Testa</h1>');
-});
-
 app.get('/products', rota.getProducts);
 app.get('/products/:id', rota.getProductsById);
 
