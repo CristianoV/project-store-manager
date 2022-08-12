df10add3b3c6f6c9a908974563daea89a7efa167
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-12 16:26:53 -0300

    refactor: deixando o codigo mais legivel

--- services/productsServices.js
+++ services/productsServices.js
@@ -3,14 +3,15 @@ const productsModel = require('../models/ProductsModels');
 const productsService = {
   getProducts: async () => {
     const data = await productsModel.getAll();
-    return data;
+    return { data, code: 200 };
   },
   getProductsById: async (id) => {
     const data = await productsModel.getById(id);
-    // if (!data) {
-    //   return { message: 'Product not found' };
-    // }
-    return data;
+    if (!data) {
+      const message = { message: 'Product not found' };
+      return { code: 404, data: message };
+    }
+    return { code: 200, data };
   },
 };
 
