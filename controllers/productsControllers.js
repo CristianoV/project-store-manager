a0e1b98f618b7c9a24302fef99b24ebaff2629a0
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-12 19:21:05 -0300

    fixed: adiciona função que retorna codigo http e resposta sobre a criação do produto

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -5,10 +5,15 @@ const productsController = {
     const { data, code } = await productsService.getProducts();
     res.status(code).json(data);
   },
-  
+
   getProductsById: async (req, res) => {
     const { id } = req.params;
     const { code, data } = await productsService.getProductsById(id);
+    res.status(code).json(data);
+  },
+  createProducts: async (req, res) => {
+    const { name } = req.body;
+    const { code, data } = await productsService.createProduct(name);
     res.status(code).json(data);
   },
 };
