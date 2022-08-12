707b290b21e5bd3c2fca9c872e4a93bde9c7241e
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-12 10:40:47 -0300

    fix: adiciona comando de criação de rota para a api

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const app = require('./app');
 require('dotenv').config();
+const rota = require('./controllers/productsControllers');
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+
+app.get('/teste', (req, res) => {
+  res.status(200).send('<h1>Testa</h1>');
+});
+
+app.get('/products', rota.getProducts);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
