efd80062e684208d57aa5acae2e64a783b3387c4
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-13 00:39:32 -0300

    feat: adiciona validações adicionar novos produtos

--- services/productsServices.js
+++ services/productsServices.js
@@ -8,15 +8,17 @@ const productsService = {
   getProductsById: async (id) => {
     const data = await productsModel.getById(id);
     if (!data) {
-      const message = { message: 'Product not found' };
-      return { code: 404, data: message };
+      return { code: 404, data: { message: 'Product not found' } };
     }
     return { code: 200, data };
   },
   createProduct: async (name) => {
-    if (!name) {
-      const message = { message: 'Name is required' };
-      return { code: 400, data: message };
+    if (!name) return { code: 400, data: { message: '"name" is required' } };
+    if (name.length < 5) {
+      return {
+        code: 422,
+        data: { message: '"name" length must be at least 5 characters long' },
+      };
     }
     const { insertId } = await productsModel.create(name);
     const data = { id: insertId, name };
