a50e21189c23adb133804802fc41c618f6ca3144
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-17 20:18:14 -0300

    feat: adiciona linha de comando que busca no mysql

--- models/ProductsSaleModels.js
+++ models/ProductsSaleModels.js
@@ -17,7 +17,40 @@ const userModel = {
       VALUES (?, ?, ?)`,
       [productId, teste1, quantity],
     );
-      return result;
+    return result;
+  },
+  saleAllProductsModel: async () => {
+    const [result] = await db.query(
+      `
+      SELECT
+        s.id as saleId,
+        s.date as date,
+        sp.product_id as productId,
+        sp.quantity as quantity
+      FROM StoreManager.sales AS s
+      LEFT JOIN StoreManager.sales_products AS sp
+      ON s.id = sp.sale_id
+      ORDER BY sale_id, productId, quantity
+      `,
+    );
+    return result;
+  },
+  saleSpecificId: async (id) => {
+    const [result] = await db.query(
+      `
+      SELECT
+        s.id as sale_id,
+        s.date as date,
+        sp.product_id as productId,
+        sp.quantity as quantity
+      FROM StoreManager.sales AS s
+      LEFT JOIN StoreManager.sales_products AS sp
+      ON s.id = sp.sale_id
+      WHERE sale_id = ${id}
+      ORDER BY sale_id, productId, quantity
+      `,
+    );
+    return result;
   },
 };
 
