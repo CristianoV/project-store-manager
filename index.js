606e408449980be24401efbb422dd5fb19be2277
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-17 16:43:59 -0300

    fix: novas bibliotecas adicionadas para tratamento de erro

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const app = require('./app');
 require('dotenv').config();
+require('express-async-errors');
+
 const rota = require('./controllers/productsControllers');
+const rotaSale = require('./controllers/productsSaleControllers');
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.get('/products', rota.getProducts);
 app.get('/products/:id', rota.getProductsById);
 app.post('/products', rota.createProducts);
+app.post('/sales', rotaSale.saleProductsCreate);
+
+app.use((err, _req, res, _next) => {
+  const { message, statusCode } = err;
+  return res.status(statusCode).json({ message });
+});
 
 app.use('*', (req, res) => {
   res.status(404).send('<h1>404</h1>');
