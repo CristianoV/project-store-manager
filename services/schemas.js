c267ecb154f65b13f3e1418cfa25c4d41de30df9
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-17 16:37:59 -0300

    feat: adiciona validações para a requisição

---
+++ services/schemas.js
@@ -0,0 +1,15 @@
+const joi = require('joi');
+
+const schemaSale = joi.array().items(
+  joi.object().keys({
+    productId: joi.number().required().messages({
+      'any.required': '400|"productId" is required',
+    }),
+    quantity: joi.number().min(1).required().messages({
+      'number.min': '422|"quantity" must be greater than or equal to 1',
+      'any.required': '400|"quantity" is required',
+    }),
+  }),
+);
+
+module.exports = { schemaSale };
