983317f92a27122208e207586c18f8150a31328a
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-17 20:20:03 -0300

    feat: adiciona função que retorna status e o objeto a ser mostrado na tela

--- controllers/productsSaleControllers.js
+++ controllers/productsSaleControllers.js
@@ -8,6 +8,16 @@ const productsController = {
     const { data, code } = await productsService.saleProducts(body);
     res.status(code).json(data);
   },
+  saleAllProducts: async (req, res) => {
+    const { data, code } = await productsService.saleAllProducts();
+    res.status(code).json(data);
+  },
+  saleSpecificId: async (req, res) => {
+    const { id } = req.params;
+    await productsService.saleIdValidator(id);
+    const { data, code } = await productsService.saleSpecificId(id);
+    res.status(code).json(data);
+  },
 };
 
 module.exports = productsController;
