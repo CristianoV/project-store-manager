77b531413a6c8eaba69ca08806948c516c94fea2
Cristiano Vieira <cristianoviieira@gmail.com>
2022-08-18 14:28:05 -0300

    feat: adiciona validação da função de atualização de produtos com Joi

--- services/schemas.js
+++ services/schemas.js
@@ -12,4 +12,14 @@ const schemaSale = joi.array().items(
   }),
 );
 
-module.exports = { schemaSale };
+const schemaProduct = joi.object({
+  id: joi.string().required().messages({
+    'any.required': '400|"id" is required',
+  }),
+  name: joi.string().min(5).required().messages({
+    'string.min': '422|"name" length must be at least 5 characters long',
+    'any.required': '400|"name" is required',
+  }),
+});
+
+module.exports = { schemaSale, schemaProduct };
